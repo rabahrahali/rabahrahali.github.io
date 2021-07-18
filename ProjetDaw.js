fc6493b891b6063eab2579b51aad5da5d5ba3bcf
Rabah Rahali <75740689+rabahrahali@users.noreply.github.com>
2021-07-18 19:48:51 +0200

    Add files via upload

---
+++ ProjetDaw.js
@@ -0,0 +1,10 @@
+function myFunction(x) {
+    if(x==1){
+        home.innerHTML = "<iframe src = 'Experience.html' frameBorder = '10'</iframe>";
+    }else if(x==2){
+        home.innerHTML = "<iframe src = 'Compitence.html' frameBorder = '10'</iframe>";
+        }else{
+        home.innerHTML = "<iframe src = 'Formation.html' frameBorder = '10'</iframe>";
+  }
+}
+
